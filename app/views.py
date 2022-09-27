553c73d0cc100bbcf4a88ac9d344a4a123488206
LucasIdalino <lucasidalino14@hotmail.com>
2022-09-26 21:51:24 -0300

    Generics views

--- app/views.py
+++ app/views.py
@@ -1,33 +1,70 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly, IsAuthenticatedOrReadOnly
+from urllib import request
 from app.models import *
 from app.serializers import *
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 
 
-class FuncionarioViewSet(ModelViewSet):
+
+class FuncionarioGenericsList(generics.ListCreateAPIView):
     queryset = Funcionario.objects.all()
     serializer_class = FuncionarioSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     
 
+class FuncionarioGenericsRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
-class ClienteModelViewSet(ModelViewSet):
+
+class ClienteGenericsList(generics.ListCreateAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     
 
-class ServicoViewSet(ModelViewSet):
+class ClienteGenericsRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class ServicoGenericsList(generics.ListCreateAPIView):
+    queryset = Servico.objects.all()
+    serializer_class = ServicoSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    
+
+class ServicoGenericsRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = Servico.objects.all()
     serializer_class = ServicoSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 
-class AtendimentoViewset(ModelViewSet):
+class AtendimentoGenericsList(generics.ListCreateAPIView):
     '''Atendimentos'''
     queryset = Atendimento.objects.all()
     serializer_class = AtendimentoSerializer
 
+#TODO  Refatorar código fazendo checagem da requisão em um query
+
+    def post(self, request):
+        print(request.data['funcionario'])
+        query = Servico.objects.filter(funcionario=request.data['funcionario'])
+       
+        if request.data['servico'] in query:
+            serializer = AtendimentoSerializer(data=request.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST, )
+
+class AtendimentoGenericsRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Atendimento.objects.all()
+    serializer_class = AtendimentoSerializer
+
+    
