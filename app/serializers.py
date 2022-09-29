f6cf15f468bcb331bd288b41ca0991775fd12d05
LucasIdalino <lucasidalino14@hotmail.com>
2022-09-28 21:56:37 -0300

    Setting create method in AtendimentoSerializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,7 @@
 from asyncore import read
 from app.models import *
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 
 
@@ -39,3 +40,20 @@ class AtendimentoSerializer(serializers.ModelSerializer):
         model = Atendimento
         fields = '__all__'    
         
+        
+    #TODO O método create não contém o parâmetro 'instance'. Neste método, deve ser verificado
+    #se o funcionário escolhido contém a habilidade desejada pela o usuário.
+
+    def create(self, validated_data, instance):
+        funcionario = validated_data['funcionario']
+        habilidade = validated_data['servico']
+        query = Funcionario.objects.all()
+
+        if validated_data['servico'] in instance.funcionario.habilidade:
+            atendimento = Atendimento.objects.create(**validated_data)
+            atendimento.save()
+            return atendimento
+        else:
+            raise ValidationError(
+                {'erro': f'O funcionário {funcionario} não tem está habilidade {habilidade}.'}
+            )
